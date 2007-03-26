8f64157c4207f3d14397c72fb7ecb26d5c9465de
don.coleman <don.coleman@a2bac47a-4d2c-0410-9b50-51df706c3945>
2007-03-26 01:01:17 +0000

    test cases

---
+++ jintrospect_test_case.py
@@ -0,0 +1,70 @@
+import unittest
+import jintrospect
+from java.lang import String
+
+class JIntrospectTestCase(unittest.TestCase):
+
+    def testGetAutoCompleteList(self):
+        s = String("Unit Test")
+        list = jintrospect.getAutoCompleteList("s.", locals())
+        self.assertNotEmpty(list)
+        self.assertContains(list, "contains")
+
+    def testGetCallTipJava(self):
+        s = String("Unit Test")
+        tip = jintrospect.getCallTipJava("s.contains(", locals())
+        self.assertEquals("contains(java.lang.CharSequence) -> boolean", tip[2])
+
+    def testGetPackageName(self):
+        package_name = jintrospect.getPackageName("import java.")
+        self.assertEquals("java", package_name)
+
+        package_name = jintrospect.getPackageName("from java.awt import")
+        self.assertEquals("java.awt", package_name)
+
+    def testCompletePackageName(self):
+        try:
+            list = jintrospect.completePackageName("bogus")
+            fail("Expecting import error.")
+        except ImportError:
+            pass
+
+        list = jintrospect.completePackageName("java")
+        self.assertNotEmpty(list)
+        self.assertContains(list, "awt")
+
+        list = jintrospect.completePackageName("java.util")
+        self.assertNotEmpty(list)
+        self.assertContains(list, "ArrayList")
+
+    def testIsPython(self):
+        s = String("Java String")
+        self.assert_(not jintrospect.ispython(s))
+
+        self.assert_(jintrospect.ispython(jintrospect))
+
+        # ideally this would pass
+        #ps = "python string"
+        #self.assert_(jintrospect.ispython(ps))
+
+        # ideally this would pass                
+        #d = {}
+        #self.assert_(jintrospect.ispython(d))
+
+
+    # note: static methods and fields are tested in static_test_case
+
+    def assertNotEmpty(self, list):
+        if list == None:
+            self.fail("list is None")
+        if len(list) < 1:
+            self.fail("list is empty")
+
+    def assertContains(self, list, value):
+        try:
+            list.index(value)
+        except ValueError:
+            self.fail("list does not contain %s" % value)
+
+if __name__ == '__main__':
+    unittest.main()
